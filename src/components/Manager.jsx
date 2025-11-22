fa638bc7583cfef0b525cf91179d36abfe7ed209
Nisarg Mistry <mistry.nisarg.24@gmail.com>
2025-11-22 18:08:51 +0530

    Responsive UI Fix

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -33,6 +33,16 @@ const Manager = () => {
             localStorage.setItem("passwords", JSON.stringify([...passwordArray, { ...form, id: uuidv4() }]))
             console.log([...passwordArray, form])
             setform({ website: "", uname: "", Password: "" })
+            toast.success('Password Saved!', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored"
+            });
         }
         else {
             toast.error('Enter Each Details', {
@@ -48,16 +58,27 @@ const Manager = () => {
         }
 
     }
-    const editPassword=(id)=>{
+    const editPassword = (id) => {
         console.log(id)
-        setform(passwordArray.filter(i=>i.id===id)[0])
-        setpasswordArray(passwordArray.filter(item=>item.id!==id))
+        setform(passwordArray.filter(i => i.id === id)[0])
+        setpasswordArray(passwordArray.filter(item => item.id !== id))
+
     }
     const deletePassword = (id) => {
         let toDel = confirm("Do you want to Delete password")
-        if(toDel){
-            setpasswordArray(passwordArray.filter(item=>item.id!==id))
-            localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item=>item.id!==id)))
+        if (toDel) {
+            setpasswordArray(passwordArray.filter(item => item.id !== id))
+            localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item => item.id !== id)))
+            toast.success('Password Deleted!', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored"
+            });
         }
     }
     const handleChange = (e) => {
@@ -69,7 +90,7 @@ const Manager = () => {
         <>
             <ToastContainer
                 position="top-right"
-                autoClose={5000}
+                autoClose={3000}
                 hideProgressBar={false}
                 newestOnTop={false}
                 closeOnClick={false}
@@ -79,68 +100,73 @@ const Manager = () => {
                 pauseOnHover
                 theme="light"
             />
-            <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-                <div className="flex lg:w-2/3 w-full sm:flex-row flex-col my-2 mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
-                    <div className="relative grow w-full">
-                        <label htmlFor="website" className="leading-7 text-sm text-white">Website</label>
-                        <input value={form.website} onChange={handleChange} type="text" id="website" name="website" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:text-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                    <div className="relative grow w-full">
-                        <label htmlFor="uname" className="leading-7 text-sm text-white">User Name</label>
-                        <input value={form.uname} onChange={handleChange} type="text" id="uname" name="uname" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:text-white focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                    <div className="relative grow w-full">
-                        <label htmlFor="Password" className="leading-7 text-sm text-white">Password</label>
-                        <input value={form.Password} onChange={handleChange} type={showPassword ? "text" : "password"} id="Password" name="Password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:text-white focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                    <button onClick={savePassword} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg flex justify-center gap-2">
-                        Add <lord-icon
-                            src="https://cdn.lordicon.com/efxgwrkc.json"
-                            trigger="hover"
-                        >
-                        </lord-icon>
-                    </button>
+            <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+            <div className="flex lg:w-2/3 w-full sm:flex-row flex-col my-2 mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+                <div className="relative grow w-full">
+                    <label htmlFor="website" className="leading-7 text-sm text-white">Website</label>
+                    <input value={form.website} onChange={handleChange} type="text" id="website" name="website" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:text-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
-                <div className="flex lg:w-2/3 w-full  mx-auto gap-2 justify-start align-middle">
-                    <input type="checkbox" name="showPassword" id="showPassword" checked={showPassword} onChange={() => setShowPassword(!showPassword)} />
-                    <span className='text-white'>Show password</span>
+                <div className="relative grow w-full">
+                    <label htmlFor="uname" className="leading-7 text-sm text-white">User Name</label>
+                    <input value={form.uname} onChange={handleChange} type="text" id="uname" name="uname" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:text-white focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
-                <div className='passwords flex flex-col lg:w-2/3 w-full sm:flex-col my-2 mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-center bg-white/15 rounded-xl overflow-hidden gap-4 py-4'>
-                    <h1 className='text-white text-3xl font-bold'>Your Passwords</h1>
-                    {passwordArray.length === 0 && <div className='text-white'>No Passwords to Show</div>}
-                    {passwordArray.length != 0 &&
-                        <table className="table-auto  text-white text-center">
-                            <thead>
-                                <tr>
-                                    <th className='border-b-2 border-t-2 p-2 text-xl'>Website</th>
-                                    <th className='border-b-2 border-t-2 p-2 text-xl'>Username</th>
-                                    <th className='border-b-2 border-t-2 p-2 text-xl'>Password</th>
-                                    <th className='border-b-2 border-t-2 p-2 text-xl'>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody className=''>
-                                {passwordArray.map((item, index) => {
-                                    return (<tr key={index}>
-                                        <td className='w-50  border-b border-white/40 p-2'>{item.website}</td>
-                                        <td className='w-50  border-b border-white/40 p-2'>{item.uname}</td>
-                                        <td className='w-50  border-b border-white/40 p-2 flex justify-center align-middle cursor-pointer' onClick={() => copyText(item.Password)}>{showPassword ? item.Password : "****"} <lord-icon
-                                            src="https://cdn.lordicon.com/iykgtsbt.json"
-                                            trigger="hover"
-                                            colors="primary:#ffffff,secondary:#ffffff"
-                                            style={{ "width": "22px" }}
-                                        >
-                                        </lord-icon></td>
-                                        <td className='w-50  border-b border-white/40 p-2'>
-                                        <button className='cursor-pointer' onClick={()=>editPassword(item.id)}>Edit</button> | 
-                                        <button className='cursor-pointer' onClick={()=>deletePassword(item.id)}>Delete</button>
-                                        </td>
-                                    </tr>)
-                                })}
-
-                            </tbody>
-                        </table>}
+                <div className="relative grow w-full">
+                    <label htmlFor="Password" className="leading-7 text-sm text-white">Password</label>
+                    <input value={form.Password} onChange={handleChange} type={showPassword ? "text" : "password"} id="Password" name="Password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:text-white focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
+                <button onClick={savePassword} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg flex justify-center gap-2">
+                    Add <lord-icon
+                        src="https://cdn.lordicon.com/efxgwrkc.json"
+                        trigger="hover"
+                    >
+                    </lord-icon>
+                </button>
             </div>
+            <div className="flex lg:w-2/3 w-full flex-row my-2 mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-0 sm:px-0 justify-start align-middle">
+                <input type="checkbox" name="showPassword" id="showPassword" checked={showPassword} onChange={() => setShowPassword(!showPassword)} />
+                <span className='text-white'> Show password</span>
+            </div>
+            <div className='passwords flex flex-col lg:w-2/3 w-full sm:flex-col my-2 mx-auto px-8 sm:w-90vw sm:space-y-0 space-y-4 sm:px-0 items-center bg-white/15 rounded-xl overflow-hidden gap-4 py-4'>
+                <h1 className='text-white text-3xl font-bold'>Your Passwords</h1>
+                {passwordArray.length === 0 && <div className='text-white'>No Passwords to Show</div>}
+                {passwordArray.length != 0 &&
+                    <table className="table-auto text-white text-center">
+                        <thead>
+                            <tr>
+                                <th className='border-b-2 border-t-2 p-2 text-xl'>Website</th>
+                                <th className='border-b-2 border-t-2 p-2 text-xl'>Username</th>
+                                <th className='border-b-2 border-t-2 p-2 text-xl'>Password</th>
+                                <th className='border-b-2 border-t-2 p-2 text-xl'>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody className=''>
+                            {passwordArray.map((item, index) => {
+                                return (<tr key={index}>
+                                    <td className='border-b border-white/40 p-2'>{item.website}</td>
+                                    <td className='border-b border-white/40 p-2'>{item.uname}</td>
+
+                                    <td className='border-b border-white/40 p-2 ' onClick={() => copyText(item.Password)}>
+                                    <div className='flex justify-center align-middle cursor-pointer'>
+                                    {showPassword ? item.Password : "****"} <lord-icon
+                                        src="https://cdn.lordicon.com/iykgtsbt.json"
+                                        trigger="hover"
+                                        colors="primary:#ffffff,secondary:#ffffff"
+                                        style={{ "width": "22px" }}
+                                        >
+                                    </lord-icon>
+                                        </div>
+                                    </td>
+                                    <td className='border-b border-white/40 p-2'>
+                                        <button className='cursor-pointer' onClick={() => editPassword(item.id)}>Edit</button><span className='invisible md:visible'> | </span>
+                                        <button className='cursor-pointer' onClick={() => deletePassword(item.id)}>Delete</button>
+                                    </td>
+                                </tr>)
+                            })}
+
+                        </tbody>
+                    </table>}
+            </div>
+
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,11 @@ const Navbar = () => {
     return (
         <nav>
             <header className="body-font bg-white">
-                <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
-                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        <span className="ml-3 text-xl">Password Manager</span>
-                    </a>
+                <div className="container mx-auto flex flex-wrap p-4 md:flex-row items-center align-middle text-center">
+                        <img src="../../../public/Favicon.png" width={25}/>
+                    <span className="flex title-font font-medium items-center text-gray-900 mb-0 ">
+                        <span className="ml-3 text-2xl">Password Manager</span>
+                    </span>
                 </div>
             </header>
         </nav>
